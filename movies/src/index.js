26d478023ddec937df50ac917c489677cf7ec099
Kate O’Neill <kateoneill@Kates-MBP.wit.ie>
2022-03-01 10:21:04 +0000

    Added Upcoming movies page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -5,7 +5,8 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage"; 
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage.js";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMovies from './pages/upcomingMovies';
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
     <Routes>
       <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+      <Route path="/movies/upcoming" element={<UpcomingMovies />} />
       <Route path="/movies/:id" element={<MoviePage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="*" element={ <Navigate to="/" /> } />
