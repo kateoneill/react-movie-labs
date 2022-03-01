58e26cebfb32183d4d70cb75f9cb9909d8237000
Kate O’Neill <kateoneill@Kates-MBP.wit.ie>
2022-03-01 09:41:08 +0000

    Full Review Page.

--- movies/src/components/templateMoviePage/index.js
+++ movies/src/components/templateMoviePage/index.js
@@ -1,32 +1,51 @@
-import React, {useState, useEffect}  from "react";
-import { useParams } from 'react-router-dom';
-import MovieDetails from "../components/movieDetails/";
-import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import React, { useState, useEffect } from "react";
+import MovieHeader from "../headerMovie";
+import Grid from "@mui/material/Grid";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { getMovieImages } from "../../api/tmdb-api";
 
-const MoviePage = (props) => {
-  const { id } = useParams();
-  const [movie, setMovie] = useState(null);
+const TemplateMoviePage = ({ movie, children }) => {
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
+    getMovieImages(movie.id).then((images) => {
+      setImages(images);
     });
-  }, [id]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
-      {movie ? (
-        <>
-          <PageTemplate movie={movie}>
-            <MovieDetails movie={movie} />
-          </PageTemplate>
-        </>
-      ) : (
-        <p>Waiting for movie details</p>
-      )}
+      <MovieHeader movie={movie} />
+
+      <Grid container spacing={5} sx={{ padding: "15px" }}>
+        <Grid item xs={3}>
+          <div sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}>
+            <ImageList 
+                cols={1}>
+                {images.map((image) => (
+                    <ImageListItem key={image.file_path} cols={1}>
+                    <img
+                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                        alt={image.poster_path}
+                    />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+          </div>
+        </Grid>
+
+        <Grid item xs={9}>
+          {children}
+        </Grid>
+      </Grid>
     </>
   );
 };
 
-export default MoviePage;
+export default TemplateMoviePage;
